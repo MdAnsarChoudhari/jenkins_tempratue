774b63f9b058faaaa7dc50bbaa8ad1799f55a71f
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2025-11-11 09:53:31 +0530

    save

---
+++ temprature.py
@@ -0,0 +1,24 @@
+import sys
+
+if len(sys.argv)==2:
+    script_name=sys.argv[0]
+    temprature=sys.argv[1]
+
+else:
+    script_name=sys.argv[0]
+    temprature=25
+    print("Input not provided, using default temprature")
+
+
+temp=int(temprature)
+if(temp<15):
+    condition="Cold"
+elif(temp>=15 and temp<=25):
+    condition="Normal"
+else:
+    condition="Hot"
+
+
+print("Script Name:",script_name)
+print("Temprature:",temprature)
+print("Condition:",condition)   
